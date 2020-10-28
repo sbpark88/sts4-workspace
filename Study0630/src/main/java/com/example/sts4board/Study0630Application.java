3559f3f57c7b30fe69a07a4d4f963312007bba07
sbpark88 <saebyulpark88@gmail.com>
2020-10-28 12:16:28 +0900

    git rebuilding

---
+++ Study0630/src/main/java/com/example/sts4board/Study0630Application.java
@@ -0,0 +1,13 @@
+package com.example.sts4board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study0630Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Study0630Application.class, args);
+	}
+
+}
---
+++ Study0630/src/test/java/com/example/sts4board/Study0630ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sts4board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study0630ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
